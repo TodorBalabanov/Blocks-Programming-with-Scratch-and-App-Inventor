cbf3b10cf3bb42249872508999ec47f81c801f7a
Todor Balabanov <todor.balabanov@gmail.com>
2022-02-05 15:46:42 +0200

    Some small fixes have been done.

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pandoc Blocks-Programming-with-Scratch-and-App-Inventor.tex -o Blocks-Programming-with-Scratch-and-App-Inventor.epub
+pandoc Blocks-Programming-with-Scratch-and-App-Inventor.tex -o Blocks-Programming-with-Scratch-and-App-Inventor.docx
+#pandoc Blocks-Programming-with-Scratch-and-App-Inventor.tex -o Blocks-Programming-with-Scratch-and-App-Inventor.pdf --pdf-engine=xelatex --variable mainfont=Calibri
