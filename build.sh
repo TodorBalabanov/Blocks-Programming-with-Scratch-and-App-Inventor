5cf0a4c60482510d443f442ec46835bacd789afa
Todor Balabanov <todor.balabanov@gmail.com>
2023-06-25 18:33:35 +0300

    Some more file formats have been added.

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-pandoc Blocks-Programming-with-Scratch-and-App-Inventor.tex -o Blocks-Programming-with-Scratch-and-App-Inventor.epub
-pandoc Blocks-Programming-with-Scratch-and-App-Inventor.tex -o Blocks-Programming-with-Scratch-and-App-Inventor.docx
-#pandoc Blocks-Programming-with-Scratch-and-App-Inventor.tex -o Blocks-Programming-with-Scratch-and-App-Inventor.pdf --pdf-engine=xelatex --variable mainfont=Calibri
+pandoc Blocks-Programming-with-Scratch-and-App-Inventor-English-Kindle.tex -o Blocks-Programming-with-Scratch-and-App-Inventor-English-Kindle.epub
